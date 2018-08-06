49ab47b38a341edd880961a39f07d758ca451dfc
Hitendra Chaudhari <hitendra.chaudhari@wolterskluwer.com>
2018-08-06 17:14:28 +0530

    Added New DotNet application

---
+++ WebApplicationSample/WebApplicationSample/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentNhibernateMVC.Models
+{
+    public class Employee
+    {
+        public virtual int Id { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string Designation { get; set; }
+    }
+}
---
+++ WebApplicationSample/WebApplicationSample/Models/NhibernateHelper.cs
@@ -0,0 +1,26 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace FluentNhibernateMVC.Models
+{
+    public class NHibernateHelper
+    {
+        public static ISession OpenSession()
+        {
+            ISessionFactory sessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2008
+                  .ConnectionString(@"Data Source=localhost\sql16;Initial Catalog=SampleDB;Integrated Security=true") 
+                              .ShowSql()
+                )
+               .Mappings(m =>
+                          m.FluentMappings
+                              .AddFromAssemblyOf<Employee>())
+                .ExposeConfiguration(cfg => new SchemaExport(cfg)
+                                                .Create(false,false))
+                .BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+    }
+}
